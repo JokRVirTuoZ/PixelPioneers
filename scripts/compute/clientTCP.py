3d121f44e89a24a3160051c98f78783063169402
Marius <marius.humeau@gmail.com>
2023-10-09 14:19:14 +0200

    clientTCP avec envoi message depuis le main

--- scripts/compute/clientTCP.py
+++ scripts/compute/clientTCP.py
@@ -1,12 +1,14 @@
 import socket as suck
 import time
 
+s = suck.socket(suck.AF_INET, suck.SOCK_STREAM)
 def ConnectToServer():
-    s = suck.socket(suck.AF_INET, suck.SOCK_STREAM)
     s.connect(("172.23.5.234", 4258))
     test = "PixelPioneer".encode()
-    close = "close".encode()
     s.send(test)
     time.sleep(1)
-    s.send(close)
-    s.close()
+
+def sendString(string):
+    s.send(string.encode("UTF-8"))
+    if string == "close":
+        s.close()
