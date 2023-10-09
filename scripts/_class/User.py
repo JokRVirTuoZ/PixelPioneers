b64cd2b3e76330ae19e5493bb3833f411060d15c
Antoine Amiaud <antoine.amiaud@reseau.eseo.fr>
2023-10-09 15:12:56 +0200

    User : Rajout d'attribut pour les initialiser

--- scripts/_class/User.py
+++ scripts/_class/User.py
@@ -1,5 +1,21 @@
 class User:
-    def __init__(self, ip, port, id):
-        self.ip = ip
-        self.port = port
+    def __init__(self, client, address, id):
+        self.client = client
+        self.ip = address[0]
+        self.port = address[1]
         self.id = id
+        self.isConnected = False
+        self.connection()
+        self.name = ""
+
+    def connection(self):
+        #compute
+        self.isConnected = True
+
+    def deconnection(self):
+        #compute
+        self.isConnected = False
+
+    def setName(self, name):
+        self.name = str(name)
+
