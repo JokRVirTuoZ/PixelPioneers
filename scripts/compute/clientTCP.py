1b4fc1e25028d56d72d001d18d289f4ed0f5dd0d
Marius <marius.humeau@gmail.com>
2023-10-09 16:38:26 +0200

    retrait du print de test dans le clientTCP

--- scripts/compute/clientTCP.py
+++ scripts/compute/clientTCP.py
@@ -4,8 +4,6 @@ import time
 s = suck.socket(suck.AF_INET, suck.SOCK_STREAM)
 def ConnectToServer():
     s.connect(("172.23.5.234", 4258))
-    test = "PixelPioneer".encode()
-    s.send(test)
     time.sleep(1)
 
 def sendString(string):
